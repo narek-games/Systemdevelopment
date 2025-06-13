c65caa2951f18f111bb6caba7abf41208fece62a
Penntora <kouki0604y@gmail.com>
2025-06-13 12:03:35 +0900

    ファイル更新

--- プログラミング/プログラミング/dbConnect.php
+++ プログラミング/プログラミング/dbConnect.php
@@ -3,11 +3,12 @@
     $host = '10.15.153.12';
     $port = 3306;
     $dbname = 'mbs';
-    $username = 'root';
+    $username = 'user';
     $password = '1212';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=$charset";
+    global $pdo;
 
     try {
         $pdo = new PDO($dsn, $username, $password);
